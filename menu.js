75559ae5e808853f8406c8f7a2ce938c58d38da4
Valux <valux.sh0p123@gmail.com>
2025-11-27 01:01:55 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
+const hamburger = document.getElementById('hamburger');
+const slideMenu = document.getElementById('slideMenu');
+const closeBtn = document.getElementById('closeBtn');
 
+hamburger.addEventListener('click', () => {
+  slideMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  slideMenu.classList.remove('active');
+});
